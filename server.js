542114035aded7d3bd9f857525794c4eff5e1a9c
Karl Eklund <localpart@gmail.com>
2012-08-08 12:24:39 +0200

    Updated application to work with current Express
    - Added package.json, which specifies versions. Run "npm install"
    - Moved some files around to more standard locations
    - Put static files in "public" and removed the manual routes
    - Updated view for current jQuery
    - Added jQuery files to public
    - Cleaned up server.js

--- server.js
+++ server.js
@@ -1,61 +1,53 @@
-var net = require('net');
-var express = require('express');
-var io = require('socket.io');
-var fs = require('fs');
- 
+var express = require('express'),
+    app = module.exports = express.createServer(),
+    io = require('socket.io').listen(app),
+    net = require('net');
 
-var app = require('express').createServer()
+// Configuration
 
-
-exports.StartSite = function() {
-   app.listen(9000);
-}
-
-app.get('/',function(req,res) { 
-   res.partial('layout.jade',{});
-})
-
-app.get('/style.css',function(req,res) {
-   
-    fs.readFile("css/style.css",function(err,d) {
-    res.end(d+"");
-    });	
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
 });
 
-app.get('/client.js',function(req,res) {
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+// Routes
+
+app.get('/', function(req, res) {
+  res.render('index', { title: 'node-mume' });
+});
+
+// Start
+
+if (require.main === module) {
+  app.listen(9000);
+  console.log("Express server listening on port %d in %s mode",
+              app.address().port, app.settings.env);
+  
+  var clients =[];
 	
-  fs.readFile("js/client.js",function(err,d) {
-     res.end(d +"");	
-  });
-});
-app.get('/settings.json',function(req,res) {
-
-   fs.readFile("js/settings.json",function(err,d) {
-      res.end(d+"");
-   })
-});
-
-exports.StartSocket = function()
-{
-   var clients =[];
-   var socket = io.listen(app);
-	
-   socket.sockets.on('connection',function(client) {
-		        
-   client.mume  = net.createConnection(4242,"mume.org"); 
-   client.mume.addListener("data",function(d){       
-        
-  var newstring =  d.toString().split("\r\n").join("<br>")
-        client.emit("data",{data:newstring});  
-   });
+  io.sockets.on('connection', function(client) {
+    client.mume = net.createConnection({ port: 4242, host: "mume.org" }); 
+    client.mume.addListener("data", function(d){
+      var newstring =  d.toString().split("\r\n").join("<br>");
+      client.emit("data", { data:newstring });  
+    });
                                
-   clients.push(client);
-   client.on('newcommand',function(d) {
-   client.mume.write(d.cmd+"","utf8",function(d) {} )}); 
-   client.on('disconnect',function() {});		
- });	
+    clients.push(client);
+    client.on('newcommand', function(d) {
+      client.mume.write(d.cmd+"","utf8", function(d) {} );
+    }); 
+    client.on('disconnect', function() {});
+  });
 }
-
-var server =require('./server.js'); 
-server.StartSite();
-server.StartSocket();
